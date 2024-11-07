0cfd3f618b0d87e72cfe23f0e201f14b3d4aab6b
salome1612 <salome.baranes@gmail.com>
2024-11-07 13:33:08 +0200

    nouveaux code multiplication

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+def multiplication(a, b):
+    resultat = a * b
+    print(f"Le résultat de la multiplication de {a} et {b} est: {resultat}")
+
+# Exemple d'utilisation
+nombre1 = float(input("Entrez le premier nombre: "))
+nombre2 = float(input("Entrez le deuxième nombre: "))
+multiplication(nombre1, nombre2)
